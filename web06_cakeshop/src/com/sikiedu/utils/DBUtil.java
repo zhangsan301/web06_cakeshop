e9232de166bec1fe069386973699bfbefb756a2f
zhangsan_301 <3152319511@qq.com>
2021-08-21 09:53:54 +0800

    这次提交的是我的蛋糕店项目。

---
+++ web06_cakeshop/src/com/sikiedu/utils/DBUtil.java
@@ -0,0 +1,30 @@
+package com.sikiedu.utils;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+
+
+public class DBUtil {
+	
+	private static DataSource ds = new ComboPooledDataSource();
+	
+	public static DataSource getDataSource() {
+		return ds;
+	}
+	
+	
+	public  static Connection getConnection() throws SQLException {
+		
+		return ds.getConnection();
+		
+	}
+	
+}
